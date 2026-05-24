36bd5ed2d3a865f1365e9d81a1009a342e8a17e1
XNOXtm <tusharamishra2006@gmail.com>
2026-05-24 23:16:03 +0530

    console ready tic tac toe

--- script.js
+++ script.js
@@ -43,19 +43,27 @@ const displayBoard = () => {
 };
 
 const Controller = (() => {
-    let currentPlayer;
-
     const p1 = Players("Tushar", "X");
     const p2 = Players("Adarsh", "O");
+    
+    const winConditions = [[0,1,2], [3,4,5], [6,7,8],
+                           [0,3,6], [1,4,7], [2,5,8],
+                           [0,4,8], [2,4,6]];
 
-    currentPlayer = p1;
+    let currentPlayer = p1;
     
     const playMove = function(position) {
         if (
             Gameboard.placeMark(position,currentPlayer.getMark())
         ) {
-            console.clear()
+            console.clear();
             displayBoard();
+
+            if (isWin()) {
+                console.log(`${currentPlayer.getName()} with mark "${currentPlayer.getMark()}" wins!`);
+            } else if (isDraw()) {
+                console.log('it is draw');
+            }
 
             if (currentPlayer == p1) {
                 currentPlayer = p2;
@@ -66,25 +74,55 @@ const Controller = (() => {
             console.log('The position in invalid or already taken!');
             console.log('RETRY!!');
         } 
+    };
+
+    const isWin = () => {
+        let board = Gameboard.getGameboard();
+
+        for (let conditions in winConditions) {
+            const condition = winConditions[conditions];
+            let a = condition[0];
+            let b = condition[1];
+            let c = condition[2];
+            
+            if ((board[a] == board[b]) && 
+                (board[b] == board[c]) && 
+                (board[a] !== "")) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+    };
+
+    const isDraw = () => {
+        let board = Gameboard.getGameboard();
+
+        for (let i = 0; i < 9; i++){
+            if (board[i] == "") {
+                return false;
+            };
+        };
+        return true;
     }
 
     return {playMove}    
 })();
 
-Controller.playMove(0); //p1
-Controller.playMove(1); //p2
-Controller.playMove(8); //p1
-Controller.playMove(1); //p2 Error!
-Controller.playMove(2); //p2
-Controller.playMove(4);
-Controller.playMove(6);
+// Controller.playMove(0); //p1
+// Controller.playMove(2); //p2
+// Controller.playMove(1); //p1
+// Controller.playMove(3); //p2 
+// Controller.playMove(4); //p1
+// Controller.playMove(7); //p2
+// Controller.playMove(5);
+// Controller.playMove(8);
+// Controller.playMove(6);
 
 
 
-//possible winning conditions 
-//1. horizonals: [0,1,2]; [3,4,5]; [6,7,8]
-//2. verticals: [0,3,6]; [2,4,7]; [2,5,8]
-//3. diagonals: [0,4,8]; [2,4,6]
+
+
 
 
 
